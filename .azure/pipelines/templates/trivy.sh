fe84040e7d078a1adb332fdfcdd310a274e29874
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-07-28 16:31:29 +0300

    Trivy run script update

--- .azure/pipelines/templates/trivy.sh
+++ .azure/pipelines/templates/trivy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+echo "Running trivy check"
 echo "$(getent hosts host.docker.internal | awk '{ print $1 }') trivy.ci" >> /etc/hosts && \
 trivy client --severity CRITICAL --remote http://trivy.ci:8083 gcr.io/${REPOSITORY}:${TAG}

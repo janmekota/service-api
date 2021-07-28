0e720d864ff65c91d8b78adc98b2177f93e91ebc
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-07-28 15:13:49 +0300

    Adding tivy run script

---
+++ .azure/pipelines/templates/trivy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "$(getent hosts host.docker.internal | awk '{ print $1 }') trivy.ci" >> /etc/hosts && \
+trivy client --severity CRITICAL --remote http://trivy.ci:8083 gcr.io/${REPOSITORY}:${TAG}
